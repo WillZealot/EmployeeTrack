edc152dddf82b0a1381dc62ffaa0507c747a50c2
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-10 15:25:16 -0400

    seeds added

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,31 @@
-DROP DATABASE IF EXISTS company_db;
+DROP DATABASE IF EXISTS company_db; -- deleted existing database of name company_db
 
-CREATE DATABASE company_db;
+CREATE DATABASE company_db; -- creates new database with name company_db
 
-USE company_db;
+USE company_db;   -- selects company_db database for use
 
-CREATE TABLE department (id, name)
+CREATE TABLE department ( -- creating a department table with an id and name
 id INT PRIMARY KEY,
-name VARCHAR(30);
+name VARCHAR(30));
 
-CREATE TABLE role (id,title,salary,department_id)
+CREATE TABLE role ( -- creating a role table with the department_id being linked to the department tables id
 id INT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
-FOREIGN KEY (department_id),
-REFERENCES department(id);
+FOREIGN KEY (department_id)
+REFERENCES department(id));
 
-CREATE TABLE employee(id,first_name,last_name,role_id,manager_id)
+CREATE TABLE employee( -- creating an employee table with the role_id being linked to the role table id
 id INT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-ON DELETE SET NULL,
-FOREIGN KEY (role_id),
+FOREIGN KEY (role_id)
 REFERENCES role(id),
-REFERENCES department(id);
-;
+
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL
+); -- on delete of manager roll it will set the value to null
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,18 +8,18 @@ VALUES
 
 INSERT INTO role (id,title,salary,department_id) -- role table insert
 VALUES
-    ( 1, "Sales Person",80000,"1"),
-    ( 2, "Lead Engineer",150000,"2"),
-    ( 3, "Software Engineer",120000,"2"),
-    ( 4, "Legal Team Lead",250000,"4"),
-    ( 5, "Lawyer",190000,"4"),
-    ( 6, "Accountant",125000,"3"),
-    ( 7, "Account Manager",160000,"3");
-    ( 8, "Sales Lead",100000,"1");
+    ( 1, "Sales Person",80000,1),
+    ( 2, "Lead Engineer",150000,2),
+    ( 3, "Software Engineer",120000,2),
+    ( 4, "Legal Team Lead",250000,4),
+    ( 5, "Lawyer",190000,4),
+    ( 6, "Accountant",125000,3),
+    ( 7, "Account Manager",160000,3),
+    ( 8, "Sales Lead",100000,1);
 
 INSERT INTO employee (id,first_name,last_name,role_id,manager_id) -- employee insert
 VALUES
-    ( 1, "John","Smith",8, NULL),
+    ( 1, "John","Smith",8,NULL),
     ( 2, "Jane","Doe",1,8), 
     ( 3, "Alex","Eubank",2,NULL),
     ( 4, "Chris","Bumstead",3,2),
