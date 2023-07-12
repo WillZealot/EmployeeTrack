ba5293366bdfe08b843047e8a5d076a1d70044c9
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-11 23:35:34 -0400

    finally fixed the error after hours in my schema the manager id didnt need to say foreign key it just needed to reference

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,25 @@ DROP DATABASE IF EXISTS company_db; -- deleted existing database of name company
 CREATE DATABASE company_db; -- creates new database with name company_db
 
 USE company_db;   -- selects company_db database for use
-
-CREATE TABLE department ( -- creating a department table with an id and name
-id INT PRIMARY KEY,
-name VARCHAR(30));
-
-CREATE TABLE role ( -- creating a role table with the department_id being linked to the department tables id
-id INT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL,
-department_id INT,
-FOREIGN KEY (department_id)
-REFERENCES department(id));
-
-CREATE TABLE employee( -- creating an employee table with the role_id being linked to the role table id
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT,
-manager_id INT,
-FOREIGN KEY (role_id)
-REFERENCES role(id),
-
-FOREIGN KEY (manager_id)
-REFERENCES employee(id)
-ON DELETE SET NULL
-); -- on delete of manager roll it will set the value to null
+-- creating a department table with an id and name
+CREATE TABLE department (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL
+);
+-- creating a role table with the department_id being linked to the department tables id
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- creating an employee table with the role_id being linked to the role table id
+CREATE TABLE employee (
+  sid INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);-- on delete of manager roll it will set the value to null
